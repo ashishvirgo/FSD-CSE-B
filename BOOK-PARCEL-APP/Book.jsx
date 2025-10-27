c1d0151bf3136ef69ca7d44ab8e118d5fc1daf17
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-10-27 15:19:58 +0530

    added book app

---
+++ BOOK-PARCEL-APP/Book.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function Book( {book}){
+    return (
+        <div className="card">
+        <img src={book.image} width="200" height="200" alt="book image"/>
+ <h3>Title: {book.title}</h3>
+          <h4>Price: ₹{book.price}/-</h4>
+          <button>Add To Cart</button>  
+        </div>
+    );
+}
+export default Book;
---
+++ REACT-VITE-APP/src/components/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Book.css'
+const Book = ({book}) => {
+  return (
+    <div className='card'>
+     <img src={book.image} width="200" height="200" alt="book image"/>
+     <h3>Title: {book.title}</h3> 
+     <h4>Price: ₹{book.price}/-</h4>
+     <button>Add to cart</button>
+    </div>
+  )
+}
+
+export default Book
